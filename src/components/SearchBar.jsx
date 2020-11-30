79d4512edb1011d30c81cfc28fda38a2cd745a3d
johnlee <johnleerighthere@gmail.com>
2020-11-30 20:12:04 +0800

    added search components

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,121 @@
+import Axios from 'axios';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import PlacesAutocomplete from 'react-places-autocomplete';
+import {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+} from 'react-places-autocomplete';
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
+
+
+
+class SearchBarComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            area: "",
+            latLng: null
+        }
+    }
+    inputChange = (event) => {
+
+        this.setState({ area: event.target.value });
+
+
+    }
+    submitData = () => {
+        console.log(this.state.latlng)
+        if (!this.state.address) {
+            console.log("enter some address")
+            toastr.error("please enter some address")
+            return false
+        }
+        if (!this.state.latLng) {
+            console.log("please select a place from the suggesstions")
+            toastr.error("please select a place from the suggesstions")
+            return false
+        }
+        Axios.post("http://localhost:5000/api/v1/clusters", { LatLng: this.state.latlng })
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    obj: { ...this.state.obj, riskArea: res.data }
+                }, () => {
+                    this.props.call([this.state.obj])
+                })
+            })
+    }
+
+    handleChange = address => {
+        this.setState({ address });
+    };
+
+    handleSelect = address => {
+        this.setState({ address })
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(res => this.setState({ latLng: res.lat + "," + res.lng, obj: { latLng: res.lat + "," + res.lng, riskArea: "", location: address } }))
+            .catch(error => console.error('Error', error));
+    };
+
+    render() {
+        return (
+            <div>
+
+                <form>
+                    <label style={{ fontWeight: 'bold', color: 'black' }}>Area to check:
+                    </label>
+                    <PlacesAutocomplete
+                        value={this.state.address}
+                        onChange={this.handleChange}
+                        onSelect={this.handleSelect}
+                    >
+                        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                            <div>
+                                <input
+                                    {...getInputProps({
+                                        placeholder: 'Search Places ...',
+                                        className: 'location-search-input',
+                                    })}
+                                />
+                                <div className="autocomplete-dropdown-container">
+                                    {loading && <div>Loading...</div>}
+                                    {suggestions.map((suggestion, i) => {
+                                        const className = suggestion.active
+                                            ? 'suggestion-item--active'
+                                            : 'suggestion-item';
+                                        // inline style for demonstration purpose
+                                        const style = suggestion.active
+                                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                        return (
+                                            <div
+                                                key={i}
+                                                {...getSuggestionItemProps(suggestion, {
+                                                    className,
+                                                    style,
+                                                })}
+                                            >
+                                                <span>{suggestion.description}</span>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        )}
+                    </PlacesAutocomplete>
+                    <Button type="button" value="Submit" variant="primary" onClick={this.submitData}>Submit</Button>
+                </form>
+            </div >
+
+
+        )
+    }
+
+}
+
+export default SearchBarComponent
